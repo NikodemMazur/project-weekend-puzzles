509df0f812bb9f46314dfa7ce9f646e3acfb4760
Nikodem Mazur <nikodem.mazur@gmail.com>
2021-07-25 14:58:55 +0200

    Create test project. Add tests for problematic classes

---
+++ tests/app/ProjectWeekendPuzzles.Tests/Security/Authentication/DefaultAuthenticationHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FluentAssertions;
+using Moq;
+using ProjectWeekendPuzzles.Security.Authentication;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProjectWeekendPuzzles.Tests.Security.Authentication
+{
+    public class DefaultAuthenticationHandlerTests
+    {
+        [Fact]
+        public async Task AuthenticateAsync_ForRolelessUser_GivesNoRoles()
+        {
+            var sut = new DefaultAuthenticationHandler();
+            var userCredentialsStub = new UserCredentials("UserName", String.Empty);
+
+            var result = await sut.AuthenticateAsync(userCredentialsStub);
+
+            result
+                .User.Roles
+                .Should().BeNullOrEmpty("becasue corresponding user has no roles included in its name");
+        }
+
+        [Fact]
+        public async Task AuthenticateAsync_ForTwoRoleUser_GivesTheseTwoRoles()
+        {
+            var sut = new DefaultAuthenticationHandler();
+            var userCredentialsStub = new UserCredentials("UserName-developer-user", String.Empty);
+
+            var result = await sut.AuthenticateAsync(userCredentialsStub);
+
+            result
+                .User.Roles
+                .Should()
+                .HaveCount(2, "becasue corresponding user has two roles included in its name")
+                .And
+                .Contain(new[] { "user", "developer" }, "because these roles are contained in the user's name");
+        }
+    }
+}
---
+++ tests/app/ProjectWeekendPuzzles.Tests/Security/Authorization/RoleAuthorizationRequirementTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Moq;
+using ProjectWeekendPuzzles.Security.Authorization;
+using ProjectWeekendPuzzles.Security.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace ProjectWeekendPuzzles.Tests.Security.Authorization
+{
+    public class RoleAuthorizationRequirementTests
+    {
+        [Fact]
+        public void HandleAuthorizationRequest_ForAuthorizedUser_GivesSuccess()
+        {
+            var allowedRoles = new[] { "Developer" };
+            var userStub = new Mock<User>(() => new User(String.Empty, allowedRoles));
+            var sut = new RoleAuthorizationRequirement(allowedRoles);
+
+            var result = sut.HandleAuthorizationRequest(userStub.Object);
+
+            result.Success.Should().BeTrue("because the user has required role");
+        }
+
+        public static IEnumerable<object[]> TheoryArgumentSet =>
+            new[] { new[] { Array.Empty<string>() },
+                    new[] { new[] { "developer" } } };
+
+        [Theory]
+        [MemberData(nameof(TheoryArgumentSet))]
+        public void HandleAuthorizationRequest_WithNoAllowedRolesProvided_GivesSuccess(IEnumerable<string> userRoles)
+        {
+            var allowedRoles = Enumerable.Empty<string>();
+            var sut = new RoleAuthorizationRequirement(allowedRoles);
+            var userStub = new Mock<User>(() => new User(String.Empty, userRoles));
+
+            var result = sut.HandleAuthorizationRequest(userStub.Object);
+
+            result.Success.Should().BeTrue("because no allowed roles have been defined");
+        }
+    }
+}
